6c80bccacccb249b6ed8c1910e6adf623d9a7ec1
Ryan Dale <dalerr@niddk.nih.gov>
2017-03-25 11:38:29 -0400

    fix multiqc paths output not in cwd

--- wrappers/wrappers/multiqc/wrapper.py
+++ wrappers/wrappers/multiqc/wrapper.py
@@ -10,9 +10,11 @@ from lcdblib.utils import utils
 outdir = os.path.dirname(snakemake.output[0])
 if not outdir:
     outdir = '.'
+basename = os.path.basename(snakemake.output[0])
 
 extra = snakemake.params.get('extra', "")
 log = snakemake.log_fmt_shell()
+
 
 # MultiQC uses Click, which in turn complains if C.UTF-8 is not set
 with utils.temp_env(dict(LC_ALL='en_US.UTF-8', LC_LANG='en_US.UTF-8')) as env:
@@ -21,7 +23,7 @@ with utils.temp_env(dict(LC_ALL='en_US.UTF-8', LC_LANG='en_US.UTF-8')) as env:
         '--quiet '
         '--outdir {outdir} '
         '--force '
-        '--filename {snakemake.output} '
+        '--filename {basename} '
         '{extra} '
         '{snakemake.params.analysis_directory} '
         '{log}'
