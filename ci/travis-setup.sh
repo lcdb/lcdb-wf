0cfdbdd7bbfdbb11d40dd3ce7dfbb08912e1c2e1
Ryan Dale <dalerr@niddk.nih.gov>
2017-03-27 17:05:44 -0400

    pin conda in root env

--- ci/travis-setup.sh
+++ ci/travis-setup.sh
@@ -15,6 +15,7 @@ conda config --add channels bioconda
 conda config --add channels lcdb
 
 ENVNAME=lcdb-wf-test
+conda install "conda<4.3"
 conda env list | grep -q $ENVNAME && conda env remove -y -n $ENVNAME
 conda create -n $ENVNAME -y python=3.5 --file requirements.txt | tee setup.log | grep -v " Time: "
 source activate $ENVNAME
