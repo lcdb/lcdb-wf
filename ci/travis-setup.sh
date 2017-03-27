c9b6373db1b6e157ca5198c999bd3c8767db881b
Ryan Dale <dalerr@niddk.nih.gov>
2017-03-27 17:35:28 -0400

    back $HOME

--- ci/travis-setup.sh
+++ ci/travis-setup.sh
@@ -4,9 +4,8 @@ set -x
 
 # Sets up travis-ci environment for testing bioconda-utils.
 curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-mkdir -p /opt/ci/miniconda
-bash Miniconda3-latest-Linux-x86_64.sh -f -b -p /opt/ci/miniconda
-export PATH=/opt/ci/miniconda/bin:$PATH
+bash Miniconda3-latest-Linux-x86_64.sh -f -b -p $HOME/anaconda
+export PATH=$HOME/anaconda/bin:$PATH
 
 # Add channels in the specified order.
 conda config --add channels conda-forge
@@ -16,7 +15,6 @@ conda config --add channels bioconda
 conda config --add channels lcdb
 
 ENVNAME=lcdb-wf-test
-conda install -y "conda<4.3"
 conda env list | grep -q $ENVNAME && conda env remove -y -n $ENVNAME
 conda create -n $ENVNAME -y python=3.5 --file requirements.txt | tee setup.log | grep -v " Time: "
 source activate $ENVNAME
