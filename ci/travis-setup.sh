ec9ec0a5042ce9600d585f6ac3424925605ba1f8
Ryan Dale <dalerr@niddk.nih.gov>
2017-03-28 12:55:18 -0400

    fixes to setup

--- ci/travis-setup.sh
+++ ci/travis-setup.sh
@@ -6,10 +6,12 @@ CONDA_DIR=$HOME/anaconda
 ENVNAME=lcdb-wf-test
 
 # Install miniconda if it doesn't already exist.
-if [[ -e $CONDA_DIR ]]; then
+if [[ ! -e $CONDA_DIR ]]; then
     curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
     bash Miniconda3-latest-Linux-x86_64.sh -f -b -p $CONDA_DIR
     export PATH="$CONDA_DIR/bin:$PATH"
+else
+    echo "$CONDA_DIR" exists, so skipping installation of miniconda
 fi
 
 # Add channels in the specified order.
@@ -22,11 +24,13 @@ conda config --add channels lcdb
 # Environment exists; update with requirements. This can happen on a buildkite
 # agent.
 if [[ conda env list | grep -q $ENVNAME ]]; then
+    echo "Environment $ENVNAME exists; updating it"
     conda install -y --file requirements.txt python=3.5
 else
     # Otherwise create a new environment (this will happen every time on
     # travis-ci). Don't show the progress for package downloads in the main
     # log, but provide setup.log as a build artifact for buildkite.
+    echo "Building environment $ENVNAME"
     conda create -n $ENVNAME -y python=3.5 --file requirements.txt \
         | tee setup.log \
         | grep -v " Time: "
@@ -34,3 +38,6 @@ fi
 
 source activate $ENVNAME
 python ci/get-data.py
+
+# buildkite expects this, so create if it doesn't exist.
+touch setup.log
