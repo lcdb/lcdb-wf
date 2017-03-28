c0021dca62c6f548d57d0a97c8fe193c941f6ffe
Ryan Dale <dalerr@niddk.nih.gov>
2017-03-28 13:19:04 -0400

    always rebuild env

--- ci/travis-setup.sh
+++ ci/travis-setup.sh
@@ -5,6 +5,10 @@ set -x
 CONDA_DIR=$HOME/anaconda
 ENVNAME=lcdb-wf-test
 
+# buildkite expects this as a build artifact; make sure it exists even if other
+# commands fail
+touch setup.log
+
 # Install miniconda if it doesn't already exist.
 if [[ ! -e $CONDA_DIR ]]; then
     curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
@@ -25,20 +29,22 @@ conda config --add channels lcdb
 # Environment exists; update with requirements. This can happen on a buildkite
 # agent.
 if conda env list | grep -q $ENVNAME; then
-    echo "Environment $ENVNAME exists; updating it"
-    conda install -y --file requirements.txt python=3.5
-else
-    # Otherwise create a new environment (this will happen every time on
-    # travis-ci). Don't show the progress for package downloads in the main
-    # log, but provide setup.log as a build artifact for buildkite.
-    echo "Building environment $ENVNAME"
-    conda create -n $ENVNAME -y python=3.5 --file requirements.txt \
-        | tee setup.log \
-        | grep -v " Time: "
+
+    # echo "Environment $ENVNAME exists; updating it"
+    # conda install -y --file requirements.txt python=3.5
+
+    # Remove the env. Seems like we're getting strange mkl symlink errors
+    conda env remove -n $ENVNAME
 fi
+
+# Otherwise create a new environment (this will happen every time on
+# travis-ci). Don't show the progress for package downloads in the main
+# log, but provide setup.log as a build artifact for buildkite.
+echo "Building environment $ENVNAME"
+conda create -n $ENVNAME -y python=3.5 --file requirements.txt \
+    | tee setup.log \
+    | grep -v " Time: "
 
 source activate $ENVNAME
 python ci/get-data.py
 
-# buildkite expects this, so create if it doesn't exist.
-touch setup.log
