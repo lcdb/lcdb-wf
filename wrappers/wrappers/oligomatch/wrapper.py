2e093e8568723eec2f7a2ea37c32bd53204cdf69
Ryan Dale <dalerr@niddk.nih.gov>
2017-04-11 11:43:54 -0400

    add oligomatch wrapper

---
+++ wrappers/wrappers/oligomatch/wrapper.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from snakemake.shell import shell
+log = snakemake.log_fmt_shell()
+shell(
+    "oligoMatch "
+    "{snakemake.input.oligos} "
+    "{snakemake.input.sequence} "
+    "{snakemake.output}"
+)
