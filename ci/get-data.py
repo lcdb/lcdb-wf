586adc582dc93d9312e1306c11228e60b82d25d5
Ryan Dale <dalerr@niddk.nih.gov>
2017-07-14 15:39:58 -0400

    back to "small" to avoid deseq2 issues
    deseq2 seems to be complaining in `rlog`, trying to see if it's because
    too few counts

--- ci/get-data.py
+++ ci/get-data.py
@@ -15,15 +15,15 @@ def _download_file(fn, dest=None):
     shell('wget -q -O- {url} > {dest}')
     return dest
 
-_download_file('rnaseq_samples/sample1/sample1.tiny_R1.fastq.gz')
-_download_file('rnaseq_samples/sample2/sample2.tiny_R1.fastq.gz')
-_download_file('rnaseq_samples/sample3/sample3.tiny_R1.fastq.gz')
-_download_file('rnaseq_samples/sample4/sample4.tiny_R1.fastq.gz')
+_download_file('rnaseq_samples/sample1/sample1.small_R1.fastq.gz')
+_download_file('rnaseq_samples/sample2/sample2.small_R1.fastq.gz')
+_download_file('rnaseq_samples/sample3/sample3.small_R1.fastq.gz')
+_download_file('rnaseq_samples/sample4/sample4.small_R1.fastq.gz')
 
 shell('mkdir -p data/rnaseq_samples/sample{{1,2,3,4}}')
 for n in [1, 2, 3, 4]:
     shell(
-        'mv rnaseq_samples/sample{n}/sample{n}.tiny_R1.fastq.gz '
+        'mv rnaseq_samples/sample{n}/sample{n}.small_R1.fastq.gz '
         'data/rnaseq_samples/sample{n}/sample{n}_R1.fastq.gz'
     )
 shell('rm -r rnaseq_samples')
