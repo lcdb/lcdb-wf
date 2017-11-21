455c646900fe3c7b0bbc32301c7d0ff0f7c995f5
Ryan Dale <dalerr@niddk.nih.gov>
2017-11-21 15:24:19 -0500

    docs, comments, improvement of example figures script

--- workflows/figures/scripts/peaks_at_promoters.py
+++ workflows/figures/scripts/peaks_at_promoters.py
@@ -3,20 +3,32 @@ import pandas
 import pybedtools
 import gffutils
 from gffutils import pybedtools_integration
-db = gffutils.FeatureDB(snakemake.input.db)
 
 outdir = os.path.dirname(snakemake.output[0])
 if not os.path.exists(outdir):
     os.makedirs(outdir)
 
-tsses = pybedtools_integration.tsses(db, as_bed6=True).saveas(os.path.join(outdir, 'tsses.bed'))
+# Based on the gffutils database, get each transcript's TSS, plus or minus 1 kb
+db = gffutils.FeatureDB(snakemake.input.db)
+tsses = (
+    pybedtools_integration.tsses(db, as_bed6=True)
+    .slop(l=1000, r=1000, s=True, genome='dm6')
+    .saveas(os.path.join(outdir, 'tsses-slop.bed'))
+)
 
 df = []
 for pk in snakemake.input.peaks:
-    name = os.path.basename(os.path.dirname(pk))
-    p = pybedtools.BedTool(pk)
-    peaks_at_tsses = p.intersect(tsses).saveas(os.path.join(outdir, name + '.bed'))
-    npks = len(peaks_at_tsses)
-    df.append(dict(name=name, npks=npks))
 
-pandas.DataFrame(df)[['name', 'npks']].to_csv(snakemake.output[0], sep='\t', index=False)
+    # Since each of the peaks files have the form '../run-name/peaks.bed', we
+    # extract "run-name" from the path to use as the label
+    label = os.path.basename(os.path.dirname(pk))
+
+    p = pybedtools.BedTool(pk)
+
+    # save the peaks found within a TSS to a separate file
+    peaks_near_tsses = p.intersect(tsses).saveas(os.path.join(outdir, label + '.bed'))
+
+    # Keep track of how many there were; this will be exported in the summary
+    df.append(dict(label=label, npks=len(peaks_near_tsses)))
+
+pandas.DataFrame(df)[['label', 'npks']].to_csv(snakemake.output[0], sep='\t', index=False)
