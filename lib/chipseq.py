70bdce028377bc903b2758afdc4535ca44b164d9
Ryan Dale <dalerr@niddk.nih.gov>
2018-01-23 16:45:52 -0500

    add doctest and more explanation for merged_input_for_ip
    also allow multiple labels per biological_material

--- lib/chipseq.py
+++ lib/chipseq.py
@@ -85,6 +85,9 @@ def merged_input_for_ip(sampletable, merged_ip):
     """
     Returns the merged input label for a merged IP label.
 
+    This is primarily used for the `fingerprint` rule, where we collect all the
+    available input BAMs together.
+
     Parameters
     ----------
 
@@ -93,15 +96,58 @@ def merged_input_for_ip(sampletable, merged_ip):
     merged_ip : str
         Label of IP to use, must be present in the `label` column of the
         sampletable.
+
+    Examples
+    --------
+    This should make more sense if we have an example to work with.....
+
+    Samples ip1 and ip2 are technical replicates. They are from a different
+    experiment than ip3 and input3, hence their different
+    biological_material.
+
+    The way we know that input1 should be paired with ip1 and ip2 is because
+    it shares the same biological material.
+
+    Compare input1 and input9. They are not technical replicates (since they
+    do not share the same `label`) but they are biological replicates because
+    they share the same biological material.
+
+    >>> from io import StringIO
+    >>> import pandas as pd
+    >>> df = pd.read_table(StringIO('''
+    ... samplename  antibody   biological_material  label
+    ... ip1         gaf        s2cell-1             s2cell-gaf-1
+    ... ip2         gaf        s2cell-1             s2cell-gaf-1
+    ... ip3         ctcf       s2cell-2             s2cell-ctcf-1
+    ... input1      input      s2cell-1             s2cell-input-1
+    ... input3      input      s2cell-2             s2cell-input-3
+    ... input9      input      s2cell-1             s2cell-input-9'''),
+    ... sep='\s+')
+
+
+    >>> merged_input_for_ip(df, 's2cell-gaf-1')
+    ['s2cell-input-1', 's2cell-input-9']
+
+    >>> merged_input_for_ip(df, 's2cell-ctcf-1')
+    ['s2cell-input-3']
+
     """
     biomaterial = sampletable.loc[sampletable['label'] == merged_ip, 'biological_material']
 
-    # note that we're printing here in addition to raising a ValueError because
+    # This is a double-check that the merged IP all comes from the same
+    # biological material.
+    #
+    # Note that we're printing here in addition to raising a ValueError because
     # when this is used in an input function to a snakemake rule, the traceback
     # is hidden
+    #
     if biomaterial.nunique() != 1:
         print('Expected a single biomaterial, found: {0}'.format(biomaterial))
         raise ValueError
+
+    # Now we find all the inputs from that biological material -- this is
+    # defined as everything sharing the biological material that has "input" as
+    # its antibody. They should also
     biomaterial = biomaterial.values[0]
     input_label = sampletable.loc[
         (sampletable['biological_material'] == biomaterial) &
@@ -109,12 +155,7 @@ def merged_input_for_ip(sampletable, merged_ip):
         'label'
     ]
 
-    if input_label.nunique() != 1:
-        raise ValueError(
-            "Expected a single input label for biological_material='{1}', "
-            "found {0}. ".format(input_label.tolist(), biomaterial))
-
-    return input_label.values[0]
+    return input_label.tolist()
 
 
 def detect_peak_format(fn):
