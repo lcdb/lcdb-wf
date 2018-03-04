44a922af0b03a53e4949a98809a950725c310d1b
Ryan Dale <dalerr@niddk.nih.gov>
2018-03-04 15:39:28 -0500

    whitespace

--- ci/get-data.py
+++ ci/get-data.py
@@ -7,6 +7,7 @@ shell.executable('/bin/bash')
 BRANCH = 'fix-multimapping-chipseq'
 URL = 'https://github.com/lcdb/lcdb-test-data/blob/{0}/data/{{}}?raw=true'.format(BRANCH)
 
+
 def _download_file(fn, dest=None):
     url = URL.format(fn)
     if dest is None:
@@ -15,6 +16,7 @@ def _download_file(fn, dest=None):
     basename = os.path.basename(fn)
     shell('wget -q -O- {url} > {dest}')
     return dest
+
 
 _download_file('rnaseq_samples/sample1/sample1.small_R1.fastq.gz', 'workflows/rnaseq/data/example_data/rnaseq_sample1.fq.gz')
 _download_file('rnaseq_samples/sample2/sample2.small_R1.fastq.gz', 'workflows/rnaseq/data/example_data/rnaseq_sample2.fq.gz')
