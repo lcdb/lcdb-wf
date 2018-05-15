0fbcfada9dcfadeee92c9c8bc23684eecb13431c
Ryan Dale <dalerr@niddk.nih.gov>
2018-05-15 11:45:22 -0400

    fix run_test.sh scripts

--- workflows/chipseq/run_test.sh
+++ workflows/chipseq/run_test.sh
@@ -1,3 +1,3 @@
 set -e
 python -m doctest ../../ci/preprocessor.py
-../ci/preprocessor.py Snakefile > Snakefile.test && snakemake -s Snakefile.test "$@"
+python ../../ci/preprocessor.py Snakefile > Snakefile.test && snakemake -s Snakefile.test "$@"
