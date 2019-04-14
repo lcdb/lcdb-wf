f80cf9caf08f9c7d1d75af342f12454be2e6bcac
Ryan Dale <dalerr@niddk.nih.gov>
2019-04-14 20:38:35 +0000

    only do updates if not using cache

--- .circleci/setup.sh
+++ .circleci/setup.sh
@@ -4,11 +4,12 @@ set -e
 MINICONDA_VER=latest
 tag="Linux"
 
-apt-get update
-apt-get install -y curl
 if ! [ -x "$(command -v conda)" ]; then
+  apt-get update
+  apt-get install -y curl
   curl -L -o miniconda.sh https://repo.continuum.io/miniconda/Miniconda3-$MINICONDA_VER-$tag-x86_64.sh
   bash miniconda.sh -b -p $CI_PROJECT_DIR/miniconda
+  conda update -y conda
 fi
 
 export PATH=$CI_PROJECT_DIR/miniconda/bin:$PATH
@@ -17,5 +18,4 @@ conda config --system --add channels defaults
 conda config --system --add channels bioconda
 conda config --system --add channels conda-forge
 
-conda update -y conda
 conda create -n lcdb-wf-test -y --file requirements.txt
