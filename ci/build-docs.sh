ac178247b1c552c66c4f871584f18b614f14b70c
Ryan Dale <dalerr@niddk.nih.gov>
2019-08-03 22:22:35 -0400

    go back to master branch

--- ci/build-docs.sh
+++ ci/build-docs.sh
@@ -30,7 +30,7 @@ DOCHTML=$DOCSOURCE/_build/html
 STAGING=/tmp/${GITHUB_USERNAME}-docs
 
 # Build docs only if travis-ci is testing this branch:
-BUILD_DOCS_FROM_BRANCH="fix-182"
+BUILD_DOCS_FROM_BRANCH="master"
 
 # ----------------------------------------------------------------------------
 #
