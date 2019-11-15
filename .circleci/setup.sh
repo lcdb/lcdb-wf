3fdb4240cb098643d9b12ff16a9bbb6c40c7b1a1
Ryan Dale <ryan.dale@nih.gov>
2019-11-15 17:19:43 -0500

    export locales

--- .circleci/setup.sh
+++ .circleci/setup.sh
@@ -2,8 +2,8 @@
 set -e
 
 apt install -y locales-all locales
-LC_ALL=en_US.utf8
-LANG=en_US.utf8
+export LC_ALL=en_US.utf8
+export LANG=en_US.utf8
 if ! conda env list | grep -q "lcdb-wf-test"; then
     echo "Setting up conda..."
     conda config --system --add channels defaults
