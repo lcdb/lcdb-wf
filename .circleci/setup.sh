d7eb4a1350e93cadf44c3a1681de60323cf0e808
Ryan Dale <ryan.dale@nih.gov>
2019-11-15 16:33:41 -0500

    use en_US.utf8

--- .circleci/setup.sh
+++ .circleci/setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e
+
+apt install -y locales-all locale
+LC_ALL=en_US.utf8
+LANG=en_US.utf8
 if ! conda env list | grep -q "lcdb-wf-test"; then
     echo "Setting up conda..."
     conda config --system --add channels defaults
