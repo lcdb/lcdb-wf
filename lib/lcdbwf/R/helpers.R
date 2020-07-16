e4ae81d1d78b184ab4cf3e45acb98d2da7d1a63e
daler <dalerr@niddk.nih.gov>
2020-07-15 22:24:34 -0400

    rewrite docs for make.dds

--- lib/lcdbwf/R/helpers.R
+++ lib/lcdbwf/R/helpers.R
@@ -359,20 +359,6 @@ DESeqDataSetFromSalmon <- function (sampleTable, design,
     return(object)
 }
 
-#' Make a list of dds objects
-#'
-#' This function takes a list of sample, design pairs, plus other info, to output
-#' a list of DESeq object.
-#'
-#' @param deseq_obj_list A named list of lists. Each list should have two to three elements:
-#'       1 should be a sampletable, 2 should be a design. The 3rd is an optional list of arguments
-#'       to pass to the DESeqDataSet constructor.
-#' @param salmon.files txi object from tximport if transcript-level counts desired; NULL forces featurecounts;
-#'        shared by all dds.
-#' @param combine.by The column to collapse technical replicates by.
-#' @param remove.version Boolean to decide whether to remove version information
-#' @return A list of dds objects.
-#'
 make.dds.list <- function(deseq_obj_list, salmon.files=NULL,
                           combine.by=FALSE, remove.version=TRUE, ...){
     make.dds <- function(design_data, salmon.files, combine.by, remove.version, ...)
@@ -388,6 +374,29 @@ make.dds.list <- function(deseq_obj_list, salmon.files=NULL,
         if(is.null(salmon.files)){
             # Load gene-level counts
            dds<- exec(DESeqDataSetFromCombinedFeatureCounts,
+#' Make a single dds object
+#'
+#' This function is intended to be applied to a list of design data.
+#'
+#' @param design_data Named list of 2 to 4 items. At least "sampletable" (the
+#'           colData, as a data.frame or tibble) and "design" (e.g., `~group`)
+#'           are required. The optional named items are "file", which is the
+#'           featureCounts file containing counts for all samples, and "args"
+#'           which is a list of arguments to be passed to the constructor
+#'           (e.g., `args=list(subset.counts=TRUE))`.
+#' @param salmon.files If you want the dds objects to be generated from salmon
+#'           counts, then provide the txi object from tximport. Otherwise,
+#'           leave as NULL to use featureCounts. The value of this argument is
+#'           used for all dds objects in the returned list (that is, the
+#'           returned list cannot have a mix of salmon and featureCounts; if
+#'           you want both you'll need to call this function twice).
+#' @param combine.by The column to collapse technical replicates by. Rows in
+#'           the sampletable that share the same value of this column will be
+#'           combined using DESeq2::collapseReplicates.
+#' @param ... Additional arguments will be passed on to the DESeq() call (e.g.,
+#'           parallel, fitType, etc)
+#' @param remove.version If TRUE, gene (or transcript) version information --
+#'           the ".1" in "ENSG0000102345.1" -- will be stripped off.
                 location,
                 sampletable=colData,
                 design=design,
