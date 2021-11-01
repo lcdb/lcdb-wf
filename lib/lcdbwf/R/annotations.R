949ab81d5fbfddc6011ff9ea55ebe619ebe9c8a9
daler <dalerr@niddk.nih.gov>
2021-11-01 14:59:23 -0400

    attach_extra can force_intersect
    Keep only the intersection across all genes in all contrasts if they
    mismatch across res objects

--- lib/lcdbwf/R/annotations.R
+++ lib/lcdbwf/R/annotations.R
@@ -91,22 +91,40 @@ get_annotation_db <- function(config, dbtype, genus_species=NULL, orgdb_key_over
 #'
 #' @return List of same results objects, but each one with additional columns
 #'   attached as specified in the config
-attach_extra <- function(res_list, config){
+attach_extra <- function(res_list, config, force_intersect){
+
+  if (missing(force_intersect)) force_intersect <- config$main$force_intersect
+  if (is.null(force_intersect)) force_intersect <- FALSE
+
   #' Ensure everything has the same gene IDs; if so then we'll grab just the
   #' gene IDs from the first one to use as our keys.
   nms <- lapply(res_list, function(x) rownames(x$res))
   if (!all(sapply(nms, function(x) all(x==nms[[1]])))){
-    stop("Gene IDs differ between results objects")
-  }
-  keys <- nms[[1]]
+    if (!force_intersect){
+      stop("Gene IDs differ between results objects. Consider using force_intersect=TRUE to only keep the common set.")
+    } else {
 
-  orgdb <- lcdbwf::get.orgdb(config$orgdb)
+      # Take advantage of utilities originally used for UpSet plots to get the
+      # set of genes found in all lists
+      warning("Keeping only the intersecting set of genes across all results lists because force_intersect=TRUE.")
+      keep <- fromList.with.names(nms)
+      keep <- rownames(keep[rowSums(keep) == ncol(keep),])
+      res_list <- lapply(res_list, function (x) {
+          x$res <- x$res[keep,]
+          x}
+      )
+    }
+  }
+
+  keys <- rownames(res_list[[1]]$res)
+
+  orgdb <- lcdbwf::get_annotation_db(config, dbtype="OrgDb")
 
   # Create a dataframe mapping gene IDs to the various configured columns
   lookups <- list()
-  for (col in config$orgdb$orgdb_columns){
-    lookups[[col]] <- mapIds(orgdb, keys=keys, column=col, keytype=config$orgdb$keytype, multiVal='first')
-    if (col %in% config$orgdb$fill){
+  for (col in config$annotation$orgdb_columns){
+    lookups[[col]] <- mapIds(orgdb, keys=keys, column=col, keytype=config$annotation$keytype, multiVal='first')
+    if (col %in% config$annotation$fill){
       lookups[[col]] <- ifelse(is.na(lookups[[col]]), keys, lookups[[col]])
     }
   }
@@ -116,11 +134,11 @@ attach_extra <- function(res_list, config){
   for (name in names(res_list)){
     res <- res_list[[name]]$res
     orig_colnames <- colnames(res)
-    for (col in config$orgdb$orgdb_columns){
+    for (col in config$annotation$orgdb_columns){
       res[[col]] <- lookups[[col]]
     }
     res$gene <- rownames(res)
-    res <- res[, c('gene', config$orgdb$orgdb_columns, orig_colnames)]
+    res <- res[, c('gene', config$annotation$orgdb_columns, orig_colnames)]
     res_list[[name]]$res <- res
   }
   return(res_list)
