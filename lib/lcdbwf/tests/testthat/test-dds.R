6bf41a15d8c62fdca44ae9e6906fa1401a4fcfa5
Matthew Menold <menoldmt@helix.nih.gov>
2024-05-22 13:07:31 -0400

    added tests for expected input to make_dds. Need to break things more next

--- lib/lcdbwf/tests/testthat/test-dds.R
+++ lib/lcdbwf/tests/testthat/test-dds.R
@@ -1,3 +1,10 @@
+library(DESeq2)
+devtools::load_all('../../../../lib/lcdbwf')
+config <- lcdbwf:::load_config('config.yaml')
+library(testthat)
+source('test-functions.R')
+
+# --------- Test strip_dotted_version_from_dds() ---------- #
 test_that("stripping gene versions", {
   dds <- DESeq2::makeExampleDESeqDataSet()
   # genes are labeled like "gene1", "gene2", etc; change them to "gene1.1",
@@ -15,3 +22,84 @@ test_that("stripping gene versions", {
   expect_equal(rownames(fixed)[1], "ENSgene1")
 
 })
+# --------------------------------------------------------- #
+
+# ---------- Test make_dds(test = 'LRT' || 'Wald') -------- #
+test_that("make_dds handles minimum required design data of sampletale,
+           and design formula, using example featurecounts table", {
+  design_data <- make_design_data()
+  make_featurecounts_file() # featurecounts.txt will be written to this directory
+  dds <- make_dds(design_data,
+                  config=config,
+                  featureCounts='featurecounts.txt',
+                  parallel=config$parallel$parallel)
+  # Check that the dds object is a DESeqDataSet (not NULL)
+  expect_true(inherits(dds, "DESeqDataSet"))
+  # Check that the WaldStatistic has been generated as Wald is the default test that
+  # is run with minimal input to make_dds()
+  expect_true(any(grepl('Wald',  colnames(mcols(dds)))))
+  expect_false(any(grepl('LRT',  colnames(mcols(dds)))))
+})
+
+test_that("make_dds handles design data where Wald test is specified explicitly", {
+  design_data <- make_design_data()
+  design_data$test <- 'Wald'
+  dds <- make_dds(design_data,
+                  config=config,
+                  featureCounts='featurecounts.txt',
+                  parallel=config$parallel$parallel)
+  # Check that the dds object is a DESeqDataSet (not NULL)
+  expect_true(inherits(dds, "DESeqDataSet"))
+  # Check that the WaldStatistic has been generated when Wald is passed to make_dds()
+  expect_true(any(grepl('Wald',  colnames(mcols(dds)))))
+  expect_false(any(grepl('LRT',  colnames(mcols(dds)))))
+})
+
+test_that("make_dds handles required design data for LRT", {
+  design_data <- make_design_data()
+  design_data$test <- 'LRT'
+  design_data$reduced_design <- ~1
+  dds <- make_dds(design_data,
+                  config=config,
+                  featureCounts='featurecounts.txt',
+                  parallel=config$parallel$parallel)
+  # Check that the dds object is a DESeqDataSet (not NULL)
+  expect_true(inherits(dds, "DESeqDataSet"))
+  # Check that the WaldStatistic has been generated
+  expect_true(any(grepl('LRT',  colnames(mcols(dds)))))
+  expect_false(any(grepl('Wald',  colnames(mcols(dds)))))
+})
+
+test_that("make_dds errors on invalid 'test' option", {
+  design_data <- make_design_data()
+  design_data$test <- "invalid_test_option"
+
+  expect_error(make_dds(design_data,
+                        config=config,
+                        featureCounts='featurecounts.txt',
+                        parallel=config$parallel$parallel),
+                        "Valid options for test are \\'Wald\\' \\(default\\) or \\'LRT\\'")
+})
+# --------------------------------------------------- #
+
+# -------------- collapseReplicates2 ---------------- #
+test_that("collapseReplicates2 collapses the three control replicates and three treatment replicates to
+           a single control row and a single treatment row in colData. Row names should still match
+           column 1.", {
+  # Setup a DESeqDataSet with replicates
+  design_data <- make_design_data()
+  # Add bio_rep to colData
+  dds <- make_dds(design_data,
+                  config=config,
+                  featureCounts='featurecounts.txt',
+                  parallel=config$parallel$parallel)
+ 
+  # Perform replicate collapsing
+  dds_collapsed <- collapseReplicates2(dds, dds$condition)
+  colData_collapsed <- as.data.frame(colData(dds_collapsed))
+  expect_equal(length(as.character(colData_collapsed$condition[colData_collapsed$condition == 'control'])), 1)
+  expect_equal(length(as.character(colData_collapsed$condition[colData_collapsed$condition == 'treatment'])), 1)
+  # Check if the first column of colData matches rownames
+  expect_equal(rownames(colData_collapsed), colData_collapsed[,1])
+})
+# --------------------------------------------------- #
