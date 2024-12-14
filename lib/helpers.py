aeca4c2dfb5ee76221e1f7280986e94cba6488b3
Brandon Fuller <brandonfuller007@gmail.com>
2024-12-13 22:05:16 -0500

    Change SRA fastq directory (#418)
    * Change SRA fastq directory
    
    Change the directory where SRA fastq files are downloaded and add the
    'orig_filename' column to the config object for each sample so that the rest of the workflow works correctly
    
    * Make code more elegant
    
    Change a nested for-loop implementation in patters_targets.py to a more elegant one-line solution and clean up some code in Snakefile
    
    * improve helper.fill_patterns
    
    add check when combining by `zip` to ensure values are all same length
    
    add more doctests
    
    ---------
    
    Co-authored-by: Ryan Dale <ryan.dale@nih.gov>

--- lib/helpers.py
+++ lib/helpers.py
@@ -34,22 +34,31 @@ def detect_layout(sampletable):
 
 def fill_patterns(patterns, fill, combination=product):
     """
-    Fills in a dictionary of patterns with the dictionary or DataFrame `fill`.
+    Fills in a dictionary of patterns with the dictionary `fill`.
 
     >>> patterns = dict(a='{sample}_R{N}.fastq')
-    >>> fill = dict(sample=['one', 'two'], N=[1, 2])
+    >>> fill = dict(sample=['one', 'two', 'three'], N=[1, 2])
     >>> sorted(fill_patterns(patterns, fill)['a'])
-    ['one_R1.fastq', 'one_R2.fastq', 'two_R1.fastq', 'two_R2.fastq']
+    ['one_R1.fastq', 'one_R2.fastq', 'three_R1.fastq', 'three_R2.fastq', 'two_R1.fastq', 'two_R2.fastq']
+
+    If using `zip` as a combination, checks to ensure all values in `fill` are
+    the same length to avoid truncated output.
+
+    This fails:
 
     >>> patterns = dict(a='{sample}_R{N}.fastq')
-    >>> fill = dict(sample=['one', 'two'], N=[1, 2])
+    >>> fill = dict(sample=['one', 'two', 'three'], N=[1, 2])
+    >>> sorted(fill_patterns(patterns, fill, zip)['a']) # doctest: +IGNORE_EXCEPTION_DETAIL
+    Traceback (most recent call last):
+    ...
+    ValueError: {'sample': ['one', 'two', 'three'], 'N': [1, 2]} does not have the same number of entries for each key
+
+    But this works:
+
+    >>> patterns = dict(a='{sample}_R{N}.fastq')
+    >>> fill = dict(sample=['one', 'one', 'two', 'two', 'three', 'three'], N=[1, 2, 1, 2, 1, 2])
     >>> sorted(fill_patterns(patterns, fill, zip)['a'])
-    ['one_R1.fastq', 'two_R2.fastq']
-
-    >>> patterns = dict(a='{sample}_R{N}.fastq')
-    >>> fill = pd.DataFrame({'sample': ['one', 'two'], 'N': [1, 2]})
-    >>> sorted(fill_patterns(patterns, fill)['a'])
-    ['one_R1.fastq', 'two_R2.fastq']
+    ['one_R1.fastq', 'one_R2.fastq', 'three_R1.fastq', 'three_R2.fastq', 'two_R1.fastq', 'two_R2.fastq']
 
     """
     # In recent Snakemake versions (e.g., this happens in 5.4.5) file patterns
@@ -64,12 +73,17 @@ def fill_patterns(patterns, fill, combination=product):
     #
     #   expand('x', zip, d=[1,2,3]) == ['x', 'x', 'x']
 
+    if combination == zip:
+        lengths = set([len(v) for v in fill.values()])
+        if len(lengths) != 1:
+            raise ValueError(f"{fill} does not have the same number of entries for each key")
+
     def update(d, u, c):
         for k, v in u.items():
             if isinstance(v, collections.abc.Mapping):
                 r = update(d.get(k, {}), v, c)
                 d[k] = r
-            else:
+            else:  # not a dictionary, so we're at a leaf
                 if isinstance(fill, pd.DataFrame):
                     d[k] = list(set(expand(u[k], zip, **fill.to_dict("list"))))
                 else:
