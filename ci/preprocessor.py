8013417fa746715ebb2d9b14e517bbb5ac771995
Ryan Dale <ryan.dale@nih.gov>
2025-01-10 23:09:30 -0500

    overhaul and simplify preprocessor

--- ci/preprocessor.py
+++ ci/preprocessor.py
@@ -7,54 +7,16 @@ We often need to run tests with specific parameters that we may not want to use
 in production. Rather than require users edit files to remove those
 test-specific patterns, here we keep the test settings commented out and only
 un-comment when running tests.
-
-First, we look for any line that matches "# [test settings]" (case insensitive,
-with optional surrounding spacing) and an optional signed integer. Any of these
-would work:
-
-    >>> assert matches('# [test settings]')
-    >>> assert matches('#[test settings]')
-    >>> assert matches('# [ test settings ]')
-    >>> assert matches('# [ test settings -1]')
-    >>> assert matches('# [ test settings +2]')
-    >>> assert matches('# [ TEST SETTINGS +2]')
-    >>> assert matches('# [ TeSt SeTTiNgS +2   ]')
-
-If a lines does not match, output it as-is.
-
-If a line matches, then uncomment it. Specifically, remove the first "#" in the
-line; if it was followed by exactly one space, then remove that too.
-
-If a line matches and a signed integer was provided, then consider it
-a relative location, and then comment-out the referred-to line. Example:
-
-    >>> preprocess('''
-    ... use this for production
-    ... # use this for tests  # [test settings -1]
-    ... '''.splitlines(True))
-    <BLANKLINE>
-    # use this for production
-    use this for tests  # [test settings -1]
-    <BLANKLINE>
-
-If the matched special string creates the first "#" in the line, then do
-nothing to that line but still respect the relative locations. Useful for just
-commenting out nearby lines for tests:
-
-    >>> preprocess('''
-    ... # [TEST SETTINGS +1]
-    ... comment out for testing'''.splitlines(True))
-    <BLANKLINE>
-    # [TEST SETTINGS +1]
-    # comment out for testing
 """
 
+
 import re
-regexp = re.compile(r'#\s?\[\s?test settings\s?(?P<rel>[-+]*\d)?\s*\]')
+
+regexp = re.compile(r"#\s?\[\s?(enable|disable) for test\s?\]")
 
 
-def matches(line):
-    return regexp.search(line.lower()) is not None
+def is_commented(line):
+    return line.strip().startswith("#")
 
 
 def comment_line(line):
@@ -66,87 +28,75 @@ def comment_line(line):
     """
     x = []
     for i, character in enumerate(line):
-        if character == ' ':
+        if character == " ":
             x.append(character)
         else:
             break
-    x.append('# ')
+    x.append("# ")
     x.extend(line[i:])
-    return ''.join(x)
+    return "".join(x)
 
 
 def uncomment_line(line):
     """
     Removes the first instance of "#" from a line; if it was followed by
-    exactly one space then remove that too.
+    exactly one space then remove that too . . . UNLESS the *only* comment is the
+    special character that triggers this behavior, in which case we do nothing.
 
     >>> assert uncomment_line('# asdf') == 'asdf'
     >>> assert uncomment_line('#asdf') == 'asdf'
     >>> assert uncomment_line('# asdf # but this should be kept') == 'asdf # but this should be kept'
     >>> assert uncomment_line('#    asdf') == '    asdf'
     >>> assert uncomment_line('  #    asdf') == '      asdf'
+    >>> assert uncomment_line('do nothing') == 'do nothing'
+    >>> assert uncomment_line('do nothing # [disable for test]') == 'do nothing # [disable for test]')
+    >>> assert uncomment_line('#uncomment # [disable for test]') == 'uncomment # [disable for test]')
     """
-    first = line.find('#')
+    first = line.find("#")
 
-    # If the first comment is the one that flag the line, then do nothing.
+    # If the first comment is the one that flagged the line, then do nothing.
     m = regexp.search(line.lower())
     if m:
         if m.start() == first:
             return line
 
-    if line[first + 1] == ' ' and line[first + 2] != ' ':
-        pattern = '# '
+    if line[first + 1] == " " and line[first + 2] != " ":
+        pattern = "# "
     else:
-        pattern = '#'
-    return line.replace(pattern, '', 1)
+        pattern = "#"
+    return line.replace(pattern, "", 1)
 
 
 def preprocess(lines):
+    result = []
 
     if isinstance(lines, str):
         lines = [lines]
 
-    # These lists will keep track of whether a line should be changed.  We need to
-    # create them ahead of time so that we can use relative indexing from line N to
-    # modify the state of lines N-1 or N+1
-    uncomment = [False for i in range(len(lines))]
-    comment = [False for i in range(len(lines))]
-
-    for i, line in enumerate(lines):
+    for line in lines:
         m = regexp.search(line.lower())
-        if m:
-            # There as at least a "[ test settings ]", so remove comment
-            uncomment[i] = True
-
-            # Figure out if there was also a relative location to uncomment,
-            # and keep track of it in the `comment` list.
-            rel = m.group('rel')
-            if rel is not None:
-                rel = int(rel)
-                comment[i + rel] = True
-
-    result = []
-    for (c, u, line) in zip(comment, uncomment, lines):
-        # E.g., in this situation, unclear what should happen:
-        #
-        #     # [test settings]
-        #     # [test settings -1]
-        #
-        if c and u:
-            raise ValueError("Line {0} is trying to be both commented and uncommented".format(line))
-        if c:
-            result.append(comment_line(line))
-        elif u:
-            result.append(uncomment_line(line))
-        else:
+        if not m:
             result.append(line)
-    print(''.join(result))
+            continue
+
+        action = m.group(1)
+        if action == "enable" and is_commented(line):
+            result.append(uncomment_line(line))
+        elif action == "disable" and not is_commented(line):
+            result.append(comment_line(line))
+        else:
+            raise ValueError(f"Inconsistent commenting and action:\n{line}")
+
+    print("".join(result))
 
 
 if __name__ == "__main__":
     import argparse
+
     ap = argparse.ArgumentParser(usage=__doc__)
-    ap.add_argument('infile', help='Input file to modify. Modified file printed to stdout.')
+    ap.add_argument(
+        "infile", help="Input file to modify. Modified file printed to stdout."
+    )
     args = ap.parse_args()
     lines = open(args.infile).readlines()
     preprocess(lines)
