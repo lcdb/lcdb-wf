8a320202ff0d6f985c4e34574725cfbd9a86e08d
Ryan Dale <115406+daler@users.noreply.github.com>
2025-10-14 22:19:09 +0000

    pep8 on postprocess.utils

--- lib/postprocess/utils.py
+++ lib/postprocess/utils.py
@@ -44,7 +44,7 @@ def extract_from_zip(tmpfiles, outfile, path_in_zip):
     shutil.rmtree(extraction_dir)
 
 
-def match_gtf_9th(tmpfiles, outfile, strmatch, optstrand = "None"):
+def match_gtf_9th(tmpfiles, outfile, strmatch, optstrand="None"):
     """
     Matches string to the 9th field of GTF and an optional strand that defaults to None;
     if the pattern is found and the provided strand match then the line is excluded
@@ -63,20 +63,25 @@ def match_gtf_9th(tmpfiles, outfile, strmatch, optstrand = "None"):
     optstrand : str
         String to match to the strand. Default is None
     """
-    regex_strmatch = re.compile(r'|'.join(strmatch))
+    regex_strmatch = re.compile(r"|".join(strmatch))
 
-    with gzip.open(outfile, 'wt') as fout:
+    with gzip.open(outfile, "wt") as fout:
         for tmpfn in tmpfiles:
-            with openfile(tmpfn, 'rt') as tmp:
+            with openfile(tmpfn, "rt") as tmp:
                 for line in tmp:
                     if line.startswith("#"):
                         fout.write(line)
                     else:
-                        toks = line.split('\t')
-                        if not (regex_strmatch.search(toks[8]) != None and toks[6] == optstrand):
+                        toks = line.split("\t")
+                        if not (
+                            regex_strmatch.search(toks[8]) != None
+                            and toks[6] == optstrand
+                        ):
                             fout.write(line)
 
+
 # match_gtf_9th(['/home/esnaultcm/Downloads/Rattus_norvegicus.Rnor_6.0.94.gtf.gz'], "test.gz", ['ENSRNOG00000046319'], '-')
+
 
 def convert_gtf_chroms(tmpfiles, outfile, conv_table):
     """
@@ -95,27 +100,31 @@ def convert_gtf_chroms(tmpfiles, outfile, conv_table):
         read lookup table, so it can be file://, a path relative to the
         snakefile, or an http://, https://, or ftp:// URL.
     """
-    lookup = pd.read_csv(
-        conv_table, sep='\t', header=None, names=('a', 'b')
-    ).set_index('a')['b'].to_dict()
+    lookup = (
+        pd.read_csv(conv_table, sep="\t", header=None, names=("a", "b"))
+        .set_index("a")["b"]
+        .to_dict()
+    )
 
-    with gzip.open(outfile, 'wt') as fout:
+    with gzip.open(outfile, "wt") as fout:
         for tmpfn in tmpfiles:
-            with openfile(tmpfn, 'rt') as tmp:
+            with openfile(tmpfn, "rt") as tmp:
                 for line in tmp:
                     if not line.startswith("#"):
-                        toks = line.split('\t')
+                        toks = line.split("\t")
                         chrom = toks[0]
                         if chrom in lookup.keys():
-                            toks[0]= lookup[chrom]
-                            line = '\t'.join(toks)
+                            toks[0] = lookup[chrom]
+                            line = "\t".join(toks)
                         else:
                             raise ValueError(
                                 'Chromosome "{chrom}" not found in conversion table '
-                                '"{conv_table}"'
-                                .format(chrom=chrom, conv_table=conv_table)
+                                '"{conv_table}"'.format(
+                                    chrom=chrom, conv_table=conv_table
+                                )
                             )
                     fout.write(line)
+
 
 def convert_fasta_chroms(tmpfiles, outfile, conv_table):
     """
@@ -135,26 +144,29 @@ def convert_fasta_chroms(tmpfiles, outfile, conv_table):
         snakefile, or an http://, https://, or ftp:// URL.
     """
 
-    lookup = pd.read_csv(
-        conv_table, sep='\t', header=None, names=('a', 'b')
-    ).set_index('a')['b'].to_dict()
+    lookup = (
+        pd.read_csv(conv_table, sep="\t", header=None, names=("a", "b"))
+        .set_index("a")["b"]
+        .to_dict()
+    )
 
-    with gzip.open(outfile, 'wt') as fout:
+    with gzip.open(outfile, "wt") as fout:
         for tmpfn in tmpfiles:
-            with openfile(tmpfn, 'rt') as tmp:
+            with openfile(tmpfn, "rt") as tmp:
                 for line in tmp:
                     if line.startswith(">"):
                         line = line.rstrip("\n")
-                        toks = line.split(' ')
+                        toks = line.split(" ")
                         chrom = toks[0].lstrip(">")
                         chrom = chrom.rstrip("\n")
                         if chrom in lookup.keys():
-                            toks[0]= ">" + lookup[chrom]
-                            line = ' '.join(toks) + "\n"
+                            toks[0] = ">" + lookup[chrom]
+                            line = " ".join(toks) + "\n"
                         else:
                             raise ValueError(
                                 'Chromosome "{chrom}" not found in conversion table '
-                                '"{conv_table}"'
-                                .format(chrom=chrom, conv_table=conv_table)
+                                '"{conv_table}"'.format(
+                                    chrom=chrom, conv_table=conv_table
+                                )
                             )
                     fout.write(line)
