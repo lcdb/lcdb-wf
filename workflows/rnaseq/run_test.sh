e6ef55410aca7ad4ea122180fd31ed986cdbaa44
Ryan Dale <115406+daler@users.noreply.github.com>
2025-10-19 02:40:08 +0000

    more simplification of references

--- workflows/rnaseq/run_test.sh
+++ workflows/rnaseq/run_test.sh
@@ -1,3 +1,3 @@
 set -e
 python -m doctest ../../ci/preprocessor.py
-python ../../ci/preprocessor.py Snakefile > Snakefile.test && snakemake -s Snakefile.test "$@"
+python ../../ci/preprocessor.py Snakefile > Snakefile.test && snakemake -s Snakefile.test --configfile ../../include/reference_config_templates/test.yaml "$@" 
